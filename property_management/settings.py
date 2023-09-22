4cf968381e03cc52d974addbccadd5d8a4f03150
WchrpWwtk <wchrp.wwtk@gmail.com>
2023-09-22 23:13:14 +0700

    Custom user roles and make new migrations.

--- property_management/settings.py
+++ property_management/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "workorders.apps.WorkordersConfig"
+    "workorders.apps.WorkordersConfig",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "auth.User"
--- workorders/models.py
+++ workorders/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
+
 # Create your models here.
+class PropertyUser(AbstractUser):
+    USER_ROLES = [
+        ("Guest", "Guest"),
+        ("Maid Supervisor", "Maid Supervisor"),
+        ("Technician Supervisor", "Technician Supervisor"),
+    ]
+
+    user_role = models.CharField(max_length=21, choices=USER_ROLES)
+    user_permissions = models.ManyToManyField(
+        "auth.Permission", blank=True, related_name="property_users"
+    )
+    groups = models.ManyToManyField(
+        Group,
+        blank=True,
+        related_name="property_users_groups",
+    )
+
+    def __str__(self):
+        return self.username
