3e8b4c34a2d07e4283304ea4420c4d9fd6a9bdd1
WchrpWwtk <wchrp.wwtk@gmail.com>
2023-09-22 22:55:22 +0700

    Start app `workorders` and delete app `apis`

--- property_management/settings.py
+++ property_management/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "apis.apps.ApisConfig"
+    "workorders.apps.WorkordersConfig"
 ]
 
 MIDDLEWARE = [
