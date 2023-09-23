14ebb54f92b6e770667f29ffe509d3eb7d7c08f1
WchrpWwtk <wchrp.wwtk@gmail.com>
2023-09-24 02:56:58 +0700

    Update router url.

--- workorders/urls.py
+++ workorders/urls.py
@@ -3,9 +3,19 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r"cleaning-work-orders", views.CleaningWorkOrderViewSet)
-router.register(r"maid-requests", views.MaidRequestViewSet)
-router.register(r"technician-request", views.TechnicianRequestViewSet)
-router.register(r"amenity-requests", views.AmenityRequestViewSet)
+router.register(
+    r"cleaning-work-orders",
+    views.CleaningWorkOrderViewSet,
+    basename="cleaning-work-orders",
+)
+router.register(r"maid-requests", views.MaidRequestViewSet, basename="maid-requests")
+router.register(
+    r"technician-requests",
+    views.TechnicianRequestViewSet,
+    basename="technician-requests",
+)
+router.register(
+    r"amenity-requests", views.AmenityRequestViewSet, basename="amenity-requests"
+)
 
 urlpatterns = [path("", include(router.urls))]
